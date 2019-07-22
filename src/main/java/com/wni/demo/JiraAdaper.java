7049f2d9038b453eedac9cee916068f050de8286
niwei_notebook <yodoo_admin@163.com>
2019-07-22 20:33:41 +0800

    judge get/pos

--- src/main/java/com/wni/demo/JiraAdaper.java
+++ src/main/java/com/wni/demo/JiraAdaper.java
@@ -2,6 +2,8 @@ package com.wni.demo;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by niwei on 2019/7/22.
  */
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.*;
 public class JiraAdaper {
     @RequestMapping("/jira/{name}")
     @ResponseBody
-    public String index(@PathVariable String name, @RequestParam int issue_id,  @RequestParam String issue_key){
+    public String index(HttpServletRequest request, @PathVariable String name, @RequestParam int issue_id,  @RequestParam String issue_key){
 
         if(null==name){
             name="jira";
         }
-        String result ="JiraAdaper  name:" +name+"    issue_id:"+ issue_id +"    issue_key:"+ issue_key;
+        String method = request.getMethod();
+
+        String result ="JiraAdaper method:" +method +  ", name:" +name+"    issue_id:"+ issue_id +"    issue_key:"+ issue_key;
         System.out.println(result);
         return result;
     }
