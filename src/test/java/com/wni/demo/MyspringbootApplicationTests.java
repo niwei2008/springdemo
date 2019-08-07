81c03a8b0f125c91c40948285f1914c4ad43daf8
niwei_notebook <yodoo_admin@163.com>
2019-08-07 16:52:47 +0800

    eureka

--- src/test/java/com/wni/demo/MyspringbootApplicationTests.java
+++ src/test/java/com/wni/demo/MyspringbootApplicationTests.java
@@ -1,12 +1,18 @@
 package com.wni.demo;
 
+import javafx.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//@RunWith(SpringRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = {Application.class})
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class MyspringbootApplicationTests {
 
 	@Test
