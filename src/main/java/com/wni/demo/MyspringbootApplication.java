fd94b5a4dd7ff1e7cde3eb6b2f47944fd9124d10
niwei_notebook <yodoo_admin@163.com>
2019-08-07 20:35:19 +0800

    add eureka client

--- src/main/java/com/wni/demo/MyspringbootApplication.java
+++ src/main/java/com/wni/demo/MyspringbootApplication.java
@@ -2,10 +2,9 @@ package com.wni.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//@EnableEurekaServer
 @SpringBootApplication
+//@EnableEurekaClient
 public class MyspringbootApplication {
 
 	public static void main(String[] args) {
