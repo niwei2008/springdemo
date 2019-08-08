fa056766ce8a2835cb6daa73d4ce6c9dd1bc175d
niwei_notebook <yodoo_admin@163.com>
2019-08-08 16:43:24 +0800

    another method

--- src/main/java/com/wni/demo/MyspringbootApplication.java
+++ src/main/java/com/wni/demo/MyspringbootApplication.java
@@ -1,12 +1,16 @@
 package com.wni.demo;
 
 //import io.micrometer.core.instrument.MeterRegistry;
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 @SpringBootApplication
 //@EnableEurekaClient
 public class MyspringbootApplication {
