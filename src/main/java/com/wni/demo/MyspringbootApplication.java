8efa0cd9e74089d9b2a9387160c7e9b3d775cf4d
niwei_notebook <yodoo_admin@163.com>
2019-08-08 15:31:30 +0800

    use another

--- src/main/java/com/wni/demo/MyspringbootApplication.java
+++ src/main/java/com/wni/demo/MyspringbootApplication.java
@@ -1,9 +1,9 @@
 package com.wni.demo;
 
-import io.micrometer.core.instrument.MeterRegistry;
+//import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+//import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
