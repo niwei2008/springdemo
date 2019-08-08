c43cb5c4f506ddef989fab0d65fa4f944d2b2e68
niwei_notebook <yodoo_admin@163.com>
2019-08-08 10:17:54 +0800

    add prometheus client

--- src/main/java/com/wni/demo/MyspringbootApplication.java
+++ src/main/java/com/wni/demo/MyspringbootApplication.java
@@ -1,7 +1,11 @@
 package com.wni.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@EnableEurekaClient
@@ -10,5 +14,10 @@ public class MyspringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyspringbootApplication.class, args);
 	}
+
+		@Bean MeterRegistryCustomizer<MeterRegistry> configurer(
+				@Value("${spring.application.name}") String applicationName) {
+				return (registry) -> registry.config().commonTags("application", applicationName);
+		}
 
 }
