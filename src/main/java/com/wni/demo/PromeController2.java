e0d48bb848c7883ea8ddd7950355f77dae696c37
niwei_notebook <yodoo_admin@163.com>
2019-08-08 15:47:46 +0800

    another method is not suit for eureka

--- src/main/java/com/wni/demo/PromeController2.java
+++ src/main/java/com/wni/demo/PromeController2.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PromeController2 {
 
     static final Counter requests = Counter.build()
-        .name("eureka_myspringboot_prome2_requests_total").help("Total requests.").register();
+        .name("myspringboot_prome2_requests_total").help("Total requests.").register();
 
     static final Gauge inprogressRequests =
-        Gauge.build().name("eureka_myspringboot_prome2_inprogress_requests").help("Inprogress requests.").register();
+        Gauge.build().name("myspringboot_prome2_inprogress_requests").help("Inprogress requests.").register();
 
     @RequestMapping("/count2")
     void processRequest1() {
