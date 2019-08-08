22328730393c8cc931890c497e61c310e6ec536b
niwei_notebook <yodoo_admin@163.com>
2019-08-08 11:34:46 +0800

    promotheus count

--- src/main/java/com/wni/demo/HelloWorldController.java
+++ src/main/java/com/wni/demo/HelloWorldController.java
@@ -18,22 +18,22 @@ public class HelloWorldController {
         if(null==name){
             name="boy";
         }
-//        //tag必须成对出现，也就是偶数个
-//        Counter counter = Counter.builder("counter")
-//            .tag("counter", "counter")
-//            .description("counter")
-//            .register(new SimpleMeterRegistry());
-//        counter.increment();
-//        counter.increment(2D);
-//        System.out.println(counter.count());
-//        System.out.println(counter.measure());
-//        //全局静态方法
-//        Metrics.addRegistry(new SimpleMeterRegistry());
-//        counter = Metrics.counter("counter", "counter", "counter");
-//        counter.increment(10086D);
-//        counter.increment(10087D);
-//        System.out.println(counter.count());
-//        System.out.println(counter.measure());
+        //tag必须成对出现，也就是偶数个
+        Counter counter = Counter.builder("counter")
+            .tag("counter", "counter")
+            .description("counter")
+            .register(new SimpleMeterRegistry());
+        counter.increment();
+        counter.increment(2D);
+        System.out.println(counter.count());
+        System.out.println(counter.measure());
+        //全局静态方法
+        Metrics.addRegistry(new SimpleMeterRegistry());
+        counter = Metrics.counter("counter", "counter", "counter");
+        counter.increment(10086D);
+        counter.increment(10087D);
+        System.out.println(counter.count());
+        System.out.println(counter.measure());
 
         return "hello world     name:" +name+"    para1:"+ para1 +"    para2:"+ para2;
     }
